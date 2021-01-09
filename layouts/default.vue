c0c73a7d749b56bc3e1d674245bc97d1280f7ad0
Marcos <sandasi.brewery@gmail.com>
2021-01-09 09:25:01 -0600

    testiando rama master

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,29 +5,7 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: "mdi-apps",
-          title: "Welcome",
-          to: "/"
-        },
-        {
-          icon: "mdi-chart-bubble",
-          title: "Inspire",
-          to: "/inspire"
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: "Vuetify.js"
-    };
-  }
-};
+export default {};
 </script>
+
+<style lang="scss"></style>
